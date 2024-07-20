341d08c0c26c695826ddf9a7794e46c64f395e7a
enard <serg.miass340@gmail.com>
2024-07-20 16:05:06 +0500

    added MediaRecorder settings

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+import time
 import datetime
+import io
+from threading import Thread
 
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+from matplotlib import pyplot as plt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -9,19 +13,63 @@ socketio = SocketIO(app)
 
 
 files = {}
+bytes_files = {}
 
 @socketio.on("init")
+def init(uuid, mime_type):
+    print("new {}".format(uuid))
+    ext = "webm" if mime_type == "video/webm" else "mp4"
+    files[uuid] = open(f"{datetime.datetime.now().isoformat()}.{ext}", "wb")
+    bytes_files[uuid] = io.BytesIO()
+
+    # Thread(target=frames_view, args=(bytes_files[uuid],)).start()
+
+@socketio.on("stop")
 def init(uuid):
-    files[uuid] = open(f"{datetime.datetime.now().isoformat()}.webm", "wb")
+    files[uuid].close()
+    files.pop(uuid)
+
 
 @socketio.on("frames")
 def farmes(uuid, data):
     files[uuid].write(data)
+
+    pos = bytes_files[uuid].tell()
+
+    bytes_files[uuid].seek(0, 2)
+    bytes_files[uuid].write(data)
+    bytes_files[uuid].seek(pos)
+
+    plt.show()
 
 @app.route('/')
 def index_page():
     return render_template('index.html')
 
 
+def frames_view(file):
+    print("imp")
+    from av import open as avopen
+    print("done")
+
+    time.sleep(3)
+    video = avopen(file, format="webm")
+
+    while True:
+        for packet in video.demux():
+            for frame in packet.decode():
+                if packet.stream.type == 'video':
+                    print(packet)
+                    print(frame)
+                    img = frame.to_ndarray(format='bgr24')
+                    time.sleep(1)
+                    plt.imshow(img)
+                    plt.pause(1)
+
+
+
 if __name__ == '__main__':
-    socketio.run(app)
+    bytes_files[1] = io.BytesIO()
+
+    print("strt")
+    socketio.run(app, "0.0.0.0")
--- static/video.js
+++ static/video.js
@@ -1,22 +1,93 @@
 var socket = io();
 
 document.addEventListener("DOMContentLoaded", () => {
+    // const canvas = document.getElementById('myChart');
+
     let video = document.getElementById("video1");
+    let startButton = document.getElementById("start-btn");
+    let stopButton = document.getElementById("stop-btn");
 
-    document.getElementById("btn").addEventListener("click", async () => {
-        let uuid = crypto.randomUUID();
-        socket.emit("init", uuid);
-        console.log(uuid);
+    let recorder;
 
-        let stream = await navigator.mediaDevices.getUserMedia({video: true, audio: true});
-        video.srcObject = stream;
-
-        let recorder = new MediaRecorder(stream, {mimeType: "video/webm"});
-
-        recorder.addEventListener("dataavailable", (data) => {
-            socket.emit("frames", uuid, data.data);
+    startButton.addEventListener("click", async () => {
+        let bpp = [];
+        let chart = new CanvasJS.Chart("chartContainer", {
+            animationEnabled: true,
+	        zoomEnabled: true,
+            data: [
+                {
+                    showInLegend: true,
+                    name: "Bytes per packet",
+                    toolTipContent: "Bytes per packet: {y}<br/>Actual bitrate: {bps} Kbit/s",
+                    color: "#369EAD",
+                    type: "line",
+                    dataPoints: bpp
+                }
+            ]
         });
 
-        recorder.start(100); // can be edited for lower latency
+        let bitrate = document.getElementById("bitrate").value;
+        let timeslice = document.getElementById("timeslice").value;
+
+        let uuid = Date.now();
+        console.log(uuid);
+
+        let stream;
+
+        try {
+            stream = await navigator.mediaDevices.getUserMedia({video: true, audio: true});
+            video.srcObject = stream;
+
+            let mimeType = MediaRecorder.isTypeSupported("video/webm") ? "video/webm" : "video/mp4";
+            console.log(mimeType);
+
+            socket.emit("init", uuid, mimeType);
+
+
+            let options = {
+                mimeType: mimeType,
+                bitsPerSecond: bitrate
+            };
+
+            recorder = new MediaRecorder(stream, options);
+            let chunkCounter = 0;
+
+            recorder.addEventListener("dataavailable", (data) => {
+                try {
+                    chunkCounter++
+                    bpp.push({
+                        x: chunkCounter,
+                        y: data.data.size,
+                        bps: (data.data.size * 8 / timeslice).toFixed(2)
+                    });
+
+                    chart.render();
+
+                    socket.emit("frames", uuid, data.data);
+                } catch (err) {
+                    alert(err);
+                    recorder.stop()
+                }
+            });
+
+            recorder.addEventListener("stop", () => {
+                video.srcObject = null;
+
+                stream.getTracks().forEach(function(track) {
+                    track.stop();
+                });
+
+                socket.emit("stop", uuid);
+            });
+
+            recorder.start(timeslice); // can be edited for lower latency
+        } catch (err){
+            alert(err);
+        }
     });
+
+    stopButton.addEventListener("click", () => {
+        recorder.stop();
+    });
+
 });
