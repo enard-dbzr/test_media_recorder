2ad13cd494995486fa41507964e4d5620def4aef
enard <serg.miass340@gmail.com>
2024-07-27 19:58:36 +0500

    made async calls to core

--- app_ws.py
+++ app_ws.py
@@ -12,20 +12,23 @@ async def hello(websocket: WebSocketServerProtocol):
         event = await websocket.recv()
         uuid = await websocket.recv()
 
+        loop = asyncio.get_event_loop()
+
         if event == "init":
             mime_type = await websocket.recv()
-            video_aggregator.start(uuid, mime_type, 1)
+            await loop.run_in_executor(None, video_aggregator.start, uuid, mime_type, 1)
         elif event == "frames":
             video_data = await websocket.recv()
-            video_aggregator.frames(uuid, video_data)
+            await loop.run_in_executor(None, video_aggregator.frames, uuid, video_data)
         elif event == "stop":
-            video_aggregator.stop(uuid)
+            loop.run_in_executor(None, video_aggregator.stop, uuid)
         elif event == "total":
-            data = video_aggregator.get_total(uuid)
+            data = await loop.run_in_executor(None, video_aggregator.get_total, uuid)
             await websocket.send(data["image"])
             await websocket.send(str(data["segments_received"]))
 
-            return
+            break
+    await websocket.close()
 
 
 async def main():
